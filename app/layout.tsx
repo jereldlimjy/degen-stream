6dcf149c4abe2d200adcd3c5e0a1a10ac1d42f36
jereldlimjy <jereldlimjy@hotmail.com>
2024-04-02 07:42:37 +0800

    update title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,19 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  // without a title, warpcast won't validate your frame
-  title: "frames.js starter",
-  description: "...",
+    // without a title, warpcast won't validate your frame
+    title: "Degen Stream",
+    description: "...",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
 }
