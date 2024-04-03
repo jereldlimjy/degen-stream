e66b3efe51344f39fe7fa4c5aad988d5193c6c59
jereldlimjy <jereldlimjy@hotmail.com>
2024-04-03 12:12:55 +0800

    fix char issue

--- app/frames/degenx/route.tsx
+++ app/frames/degenx/route.tsx
@@ -27,7 +27,8 @@ export const POST = frames(async (ctx: any) => {
                 </div>
                 <div tw="flex justify-center">
                     <span tw="text-3xl text-center text-rose-600 mt-12">
-                        Sorry for the bad UX, there's a limit of 4 buttons :'(
+                        Sorry for the bad UX, there's a limit of 4 buttons :'
+                        {"("}
                     </span>
                 </div>
             </div>
