af0ffd3e80914d7a5fa4c589c5341b3532f1244c
jereldlimjy <jereldlimjy@hotmail.com>
2024-04-04 20:20:27 +0800

    add airstack validator

--- app/frames/balance/route.tsx
+++ app/frames/balance/route.tsx
@@ -8,6 +8,7 @@ import {
     DEGEN_CONTRACT_ADDRESS,
     DEGENX_CONTRACT_ADDRESS,
 } from "../../constants";
+import { init, validateFramesMessage } from "@airstack/frames";
 import * as dotenv from "dotenv";
 
 dotenv.config();
@@ -17,6 +18,10 @@ dotenv.config();
 // const DEGENX_CONTRACT_ADDRESS = "0x131780640EDf9830099AAc2203229073d6D2FE69";
 
 export const POST = frames(async (ctx: any) => {
+    const payload = await ctx.request.json();
+    init(process.env.AIRSTACK_API_KEY ?? "");
+    await validateFramesMessage(payload);
+
     const { username } = ctx.message.requesterUserData;
     const profileImage = ctx.message.requesterUserData?.profileImage ?? "";
     const walletAddress = ctx.message.requesterVerifiedAddresses[0];
--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // prevent double render on dev mode, which causes 2 frames to exist
-  reactStrictMode: false,
-  images: {
-    remotePatterns: [
-      {
-        hostname: "*",
-        protocol: "http",
-      },
-      {
-        hostname: "*",
-        protocol: "https",
-      },
-    ],
-  },
+    webpack: (config, { dev, isServer, webpack, nextRuntime }) => {
+        config.module.rules.push({
+            test: /\.node$/,
+            use: [
+                {
+                    loader: "nextjs-node-loader",
+                },
+            ],
+        });
+        return config;
+    },
+    // prevent double render on dev mode, which causes 2 frames to exist
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [
+            {
+                hostname: "*",
+                protocol: "http",
+            },
+            {
+                hostname: "*",
+                protocol: "https",
+            },
+        ],
+    },
 };
 
 export default nextConfig;
